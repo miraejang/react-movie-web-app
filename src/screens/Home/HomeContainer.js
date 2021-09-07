f22871ff94063de279e49e6ee381a5d087046552
miraejang <mirae611@nate.com>
2021-09-07 18:27:14 +0900

    add checkbox on home

--- src/screens/Home/HomeContainer.js
+++ src/screens/Home/HomeContainer.js
@@ -1,3 +1,4 @@
+import { movieApi, tvApi } from 'api';
 import React from 'react';
 import HomePresenter from './HomePresenter';
 
@@ -6,7 +7,17 @@ class HomeContainer extends React.Component {
     super(props);
 
     this.state = {
-      movies: [],
+      loading: true,
+      error: null,
+      movies_popular: null,
+      movies_nowPlaying: null,
+      movies_upcoming: null,
+      movies_topRated: null,
+      shows_popular: null,
+      shows_airingToday: null,
+      shows_onAir: null,
+      shows_topRated: null,
+      checkedList: ['movies', 'popular'],
     };
   }
 
@@ -16,11 +27,51 @@ class HomeContainer extends React.Component {
 
   getMovies = async () => {
     try {
-    } catch (error) {}
+      const {
+        data: { results: movies_popular },
+      } = await movieApi.popular();
+      const {
+        data: { results: movies_nowPlaying },
+      } = await movieApi.nowPlaying();
+      const {
+        data: { results: movies_upcoming },
+      } = await movieApi.upcoming();
+      const {
+        data: { results: movies_topRated },
+      } = await movieApi.topRated();
+      const {
+        data: { results: shows_popular },
+      } = await tvApi.popular();
+      const {
+        data: { results: shows_airingToday },
+      } = await tvApi.airingToday();
+      const {
+        data: { results: shows_onAir },
+      } = await tvApi.onAir();
+      const {
+        data: { results: shows_topRated },
+      } = await tvApi.topRated();
+      this.setState({
+        movies_popular,
+        movies_nowPlaying,
+        movies_upcoming,
+        movies_topRated,
+        shows_popular,
+        shows_airingToday,
+        shows_onAir,
+        shows_topRated,
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
+  handdleChange = e => console.log('checked change', e.target.checked);
+
   render() {
-    return <HomePresenter />;
+    return <HomePresenter {...this.state} handdleChange={this.handdleChange} />;
   }
 }
 
--- src/screens/Home/HomePresenter.js
+++ src/screens/Home/HomePresenter.js
@@ -1,9 +1,86 @@
+import Loader from 'components/Loader';
 import React from 'react';
 
-const HomePresenter = () => {
+const HomePresenter = ({
+  loading,
+  error,
+  movies_popular,
+  movies_nowPlaying,
+  movies_upcoming,
+  movies_topRated,
+  shows_popular,
+  shows_airingToday,
+  shows_onAir,
+  shows_topRated,
+  checkedList,
+  handdleChange,
+}) => {
   return (
     <div>
-      <h1>Home!</h1>
+      <form>
+        <div>
+          <span>
+            <input
+              type="checkbox"
+              id="movies"
+              name="movies"
+              defaultChecked={checkedList.includes('movies')}
+              onChange={handdleChange}
+            />
+            <label htmlFor="movies">Movies</label>
+          </span>
+          <span>
+            <input
+              type="checkbox"
+              id="shows"
+              name="shows"
+              defaultChecked={checkedList.includes('shows')}
+              onChange={handdleChange}
+            />
+            <label htmlFor="shows">TV Shows</label>
+          </span>
+        </div>
+        <div>
+          <span>
+            <input
+              type="checkbox"
+              id="popular"
+              name="popular"
+              defaultChecked={checkedList.includes('popular')}
+            />
+            <label htmlFor="popular">Popualr</label>
+          </span>
+          <span>
+            <input type="checkbox" id="nowPlaying" name="nowPlaying" />
+            <label htmlFor="nowPlaying">Now Playing</label>
+          </span>
+          <span>
+            <input type="checkbox" id="upcoming" name="upcoming" />
+            <label htmlFor="upcoming">Upcoming</label>
+          </span>
+          <span>
+            <input type="checkbox" id="topRated" name="topRated" />
+            <label htmlFor="topRated">Top Rated</label>
+          </span>
+          <span>
+            <input type="checkbox" id="airingToday" name="airingToday" />
+            <label htmlFor="airingToday">Airing Today</label>
+          </span>
+          <span>
+            <input type="checkbox" id="onAir" name="onAir" />
+            <label htmlFor="onAir">On The Air</label>
+          </span>
+        </div>
+      </form>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {checkedList &&
+            checkedList.length > 0 &&
+            checkedList.map(e => <h1>{e}</h1>)}
+        </>
+      )}
     </div>
   );
 };
