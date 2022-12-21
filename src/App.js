3b610162152cd0e38ae41d66ac1398074f5c1dc7
miraejang <mirae611@nate.com>
2022-12-22 01:20:34 +0900

    react-scripts version update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import GlobalStyle from './GlobalStyle';
-import Header from './Header';
+import GlobalStyle from './components/GlobalStyle';
+import Header from './components/Header';
 import Home from 'screens/Home';
 import Movies from 'screens/Movies';
 import TV from 'screens/TV';
